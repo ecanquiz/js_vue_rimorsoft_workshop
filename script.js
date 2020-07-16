36ab5fded4cf03f7d07f31a1d933a1dc7c51c4e6
Ernesto Canquiz <fdsoil123@gmail.com>
2020-07-16 09:33:56 -0400

    1° commit. Lesson 1 - Welcome to the VUE.js Workshop

---
+++ script.js
@@ -0,0 +1,12 @@
+
+new Vue({
+  el: '#appVue',
+  data: {
+      lists:[
+          {keep: 'uno', completed: false},
+          {keep: 'dos', completed: true},
+          {keep: 'tres', completed: false},
+      ],
+  }
+})
+
