abdab0b7b7ce49f8f4711e06199b3e2231daef8c
Ernesto Canquiz <fdsoil123@gmail.com>
2020-07-16 11:41:12 -0400

    Changed the (class = disabled) statement to (style = color: gray), as the bootstrap class appears to lock the item once it is disabled. Therefore, it did not allow re-enabling it. Also the file vue.min.js was deleted.

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Lesson 1 - Welcome to the VUE.js Workshop
 
 Lesson 2 - Project Structure with HTML and BOOTSTRAP
 
-Lesson 3 -Dynamic list with VUEjs,  V-FOR, V-BIND
+Lesson 3 - Dynamic list with VUEjs, V-FOR, V-BIND
+
+Lesson 4 - V-IF, V-ELSE directives - VUEjs
+
+Lesson 5 - Summary of VUEjs tutorial - Workshop Project
--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-
 new Vue({
-  el: '#appVue',
-  data: {
-      lists:[
-          {keep: 'uno', completed: false},
-          {keep: 'dos', completed: true},
-          {keep: 'tres', completed: false},
-      ],
-  }
+    el: '#appVue',
+    data: {
+        lists:[
+            {keep: 'uno', completed: false},
+            {keep: 'dos', completed: true},
+            {keep: 'tres', completed: false},
+        ],
+        newKeep: ''
+    },
+    methods: {
+        addKeep: function () {
+            this.lists.push({keep: this.newKeep, completed: false});
+            this.newKeep = '';
+        }
+    }  
 })
+
+//vue how enabled an element disabled
+//https://stackoverflow.com/questions/38085180/how-to-disable-input-conditionally-in-vue-js
+//https://forum.vuejs.org/t/how-to-enabled-a-disabled-button-when-there-are-texts-in-the-input/18057/5
+//https://jsfiddle.net/willywg/2g7m5qy5/
 
